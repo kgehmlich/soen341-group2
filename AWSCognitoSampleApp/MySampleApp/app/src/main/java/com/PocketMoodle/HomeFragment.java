ca592ce3649cdadaf53f804aa1566cba6d892bbf
Winterhart <m.user.work@gmail.com>
2017-02-19 14:46:25 -0500

    A new method have been created GetAllClassRegisteredIn inside ./Service/GetAllClass, the method is returning List<String> All Class register Small refactor and changes on InserUserDetails... Addition of thread and call inside HomeFragment class

--- AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/HomeFragment.java
+++ AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/HomeFragment.java
@@ -3,16 +3,22 @@ package com.PocketMoodle;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.PocketMoodle.Services.GetAllClass;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
-
+    private static final String TAG = "FromHomeFragment";
+    public static List<String> ListOfClassUserIsIn = new ArrayList<String>();
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -21,6 +27,27 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        /**
+         * ListOfClassIsIn is a list of all class the user is registered in...
+         */
+        Runnable runnable = new Runnable() {
+            public void run() {
+                GetAllClass GetClassForUser = new GetAllClass();
+                ListOfClassUserIsIn = GetClassForUser.GetAllClassRegisteredIn();
+
+
+            }
+        };
+        Thread ThreadGetClassForAUser = new Thread(runnable);
+        try{
+            ThreadGetClassForAUser.start();
+        }
+        catch (Exception exp){
+            Log.e(TAG, exp.getMessage().toString());
+        }
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
