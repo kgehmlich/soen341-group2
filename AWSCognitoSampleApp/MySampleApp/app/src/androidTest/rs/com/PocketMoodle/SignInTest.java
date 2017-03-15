40949eef9ee95276fde4ddac85f2587fe1b32207
Mohamed Lemine <momo_lebest@hotmail.fr>
2017-03-15 11:23:11 -0400

    Add a test for signing in using the espresso framwork

---
+++ AWSCognitoSampleApp/MySampleApp/app/src/androidTest/rs/com/PocketMoodle/SignInTest.java
@@ -0,0 +1,82 @@
+package com.PocketMoodle;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SignInTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
+
+    @Test
+    public void signInTest() {
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.signIn_editText_email), isDisplayed()));
+        editText.perform(replaceText("mewtrandell"), closeSoftKeyboard());
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.signIn_editText_password), isDisplayed()));
+        editText2.perform(replaceText("mewtrandell"), closeSoftKeyboard());
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.signIn_imageButton_login), isDisplayed()));
+        imageButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withText("Home"),
+                        childAtPosition(
+                                allOf(withId(R.id.nav_action),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("Home")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
