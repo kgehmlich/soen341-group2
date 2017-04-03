60c45425ba7b3bd238a81fe609893c0e87b6f034
Yufeng Ding <aimixiu1993@gmail.com>
2017-04-03 17:29:04 -0400

    Add the Grades section for the Grades feature
    This commit will add a button called Grades on each class's fragment and will redirect user to the grades fragment.
    The grades fragment is currently empty, next step is to implement the feature.

---
+++ AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/GradesFragment.java
@@ -0,0 +1,28 @@
+package com.PocketMoodle;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yufeng on 2017-04-03.
+ */
+
+public class GradesFragment extends Fragment {
+
+    public GradesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_grades, container, false);
+
+        return view;
+    }
+
+}
