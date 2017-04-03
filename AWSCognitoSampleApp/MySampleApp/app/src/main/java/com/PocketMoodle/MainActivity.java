9061c1db44d8cc35d54fd5ca99b877c6b1f3656f
Winterhart <m.user.work@gmail.com>
2017-04-03 11:36:21 -0400

    Revert "Merge pull request #141 from kgehmlich/david-grades"
    This reverts commit 9c8b0c1ee7267a54f07a6ab7592e047e3a017b85, reversing
    changes made to 47955cfe1c1759d77af975704b78ada05082321d.

--- AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/MainActivity.java
+++ AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/MainActivity.java
@@ -8,8 +8,6 @@
 //
 package com.PocketMoodle;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -33,7 +31,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.PocketMoodle.util.JWTUtils;
@@ -80,8 +77,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int IMAGE_UPLOAD_REQUEST=42;
     Uri imageUri;
     private ImageButton imgButton;
-    private RadioButton btn;
-
 
     /**
      * Initializes the Toolbar for use with the activity.
@@ -216,8 +211,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivityForResult(intent, IMAGE_UPLOAD_REQUEST);
                             }
          });
-         //****end of Profile Picture code
-
+        //end of Profile Picture code
 
     }
 
@@ -275,7 +269,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             finish();
             return;
         }
-
 
         // ... add any other button handling code here ...
 
