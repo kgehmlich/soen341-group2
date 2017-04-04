22a63f1b7b25fe81d28182e2143009bc1eb86888
Mohamed Lemine <momo_lebest@hotmail.fr>
2017-04-03 22:30:29 -0400

    Add test for the announcement feature

---
+++ AWSCognitoSampleApp/MySampleApp/app/src/androidTest/java/com/PocketMoodle/AnnouncementTest.java
@@ -0,0 +1,211 @@
+package com.PocketMoodle;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.AdapterView;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AnnouncementTest {
+
+    //This rule makes the test start at the SignIn page (in other words it skips the splash screen)
+    @Rule
+    public ActivityTestRule<SignInActivity> mActivityTestRule = new ActivityTestRule<>(SignInActivity.class);
+
+    @Test
+    public void announcement() {
+        //Not necessary, just a precaution for travis
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        //Sign in with username and password
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.signIn_editText_email), isDisplayed()));
+        editText2.perform(replaceText("mewtrandell"), closeSoftKeyboard());
+
+        ViewInteraction editText3 = onView(
+                allOf(withId(R.id.signIn_editText_password), isDisplayed()));
+        editText3.perform(replaceText("mewtrandell"), closeSoftKeyboard());
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.signIn_imageButton_login), isDisplayed()));
+        imageButton.perform(click());
+
+        //This wait is necessary since when you sign in a message pops up.
+        //If it wasn't here the test will look for views right away and fail
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //This next part just adds a class to the list as a TA (sort of a precondition for accessing the announcements)
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open"),
+                        withParent(withId(R.id.nav_action)),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatCheckedTextView = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Add Class"), isDisplayed()));
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction appCompatRadioButton = onView(
+                allOf(withId(R.id.radioButton), withText("I am a TA"), isDisplayed()));
+        appCompatRadioButton.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.button), withText("Submit"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Open"),
+                        withParent(withId(R.id.nav_action)),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction appCompatCheckedTextView2 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Home Page"), isDisplayed()));
+        appCompatCheckedTextView2.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.text1), withText("COEN 341"),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+
+        //Add an announcement
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.add_discussion_group), withText("+"),
+                        withParent(withId(R.id.announcementSection)),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.announcementTitle), isDisplayed()));
+        appCompatEditText.perform(replaceText("a test"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.announcementDescription), isDisplayed()));
+        appCompatEditText2.perform(replaceText("testing"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.okAddAnnouncement), withText("Okay"), isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.text1), withText("a test"),
+                        isDisplayed()));
+
+        //Check that it has been added and displyed on the screen
+        textView.check(matches(withText("a test")));
+
+        //Click on the announcement that was just added to check if description is correct
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withId(android.R.id.text1), withText("a test"),
+                        isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.setAnnouncementDescription), withText("testing"),
+                        isDisplayed()));
+        //Check if description is correct
+        textView2.check(matches(withText("testing")));
+
+
+        //This last part just removes the announcement so that the each time the
+        //test is run, a new announcement is added
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withContentDescription("Open"),
+                        withParent(withId(R.id.nav_action)),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction appCompatCheckedTextView3 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Home Page"), isDisplayed()));
+        appCompatCheckedTextView3.perform(click());
+
+        ViewInteraction appCompatTextView3 = onView(
+                allOf(withId(android.R.id.text1), withText("COEN 341"),
+                        isDisplayed()));
+        appCompatTextView3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.remove_discussion_group), withText("-"),
+                        withParent(withId(R.id.announcementSection)),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+
+
+        //Click on the spinner
+        ViewInteraction appCompatTextView4 = onView(
+                allOf(withId(android.R.id.text1), isDisplayed()));
+        appCompatTextView4.perform(click());
+
+        //This next line is to select the announcement from the spinner
+        onView(withText("a test")).inRoot(isPlatformPopup()).perform(click());
+
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(android.R.id.button1), withText("Remove")));
+        appCompatButton5.perform(scrollTo(), click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
