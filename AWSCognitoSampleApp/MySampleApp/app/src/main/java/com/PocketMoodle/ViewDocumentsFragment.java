e3c4be9560fe6213333a21e97df36479562a23c8
Kyle Gehmlich <kgehmlich@gmail.com>
2017-04-04 15:55:00 -0400

    Implement document download
    - Create item click listener on list view
    - Check for permission to write to external storage before downloading

--- AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/ViewDocumentsFragment.java
+++ AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/ViewDocumentsFragment.java
@@ -2,15 +2,20 @@ package com.PocketMoodle;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.PocketMoodle.Services.DocumentServices;
 
+import java.io.File;
 import java.util.ArrayList;
 
 
@@ -42,6 +47,20 @@ public class ViewDocumentsFragment extends Fragment {
     }
 
 
+    public void onDocumentClick(String documentName) {
+        Toast.makeText(getContext(), documentName, Toast.LENGTH_SHORT).show();
+
+        // Download file using DocumentServices.download()
+        if (isExternalStorageWritable()) {
+            // Create file to be saved (stores each class's documents in a separate folder)
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), className+"/"+documentName);
+
+            DocumentServices documentServices = new DocumentServices(getContext());
+            documentServices.download(documentName, className, file);
+        }
+    }
+
+
     private class FetchClassDocumentList extends AsyncTask<Void, Void, Void> {
 
         private ArrayList<String> classDocumentList;
@@ -57,6 +76,9 @@ public class ViewDocumentsFragment extends Fragment {
         }
 
         protected void onPostExecute(Void unused) {
+            // ********************************************************
+            // Set listView adapter to display list of documents
+            // ********************************************************
             ArrayAdapter<String> docAdapter = new ArrayAdapter<String>(
                     getContext(),
                     android.R.layout.simple_list_item_1,
@@ -64,6 +86,31 @@ public class ViewDocumentsFragment extends Fragment {
             ListView docListView = (ListView) v.findViewById(R.id.documentListView);
 
             docListView.setAdapter(docAdapter);
+
+
+            // ********************************************************
+            // Set listView click listener to download documents
+            // ********************************************************
+            docListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    onDocumentClick(((TextView)view).getText().toString());
+                }
+            });
         }
+    }
+
+
+    /**
+     * From developer.android.com
+     * Checks whether or not external storage is available for writing.
+     * @return
+     */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
