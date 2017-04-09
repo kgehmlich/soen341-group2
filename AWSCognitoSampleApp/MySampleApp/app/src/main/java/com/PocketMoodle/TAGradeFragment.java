c11858c15fd6bcd5fa5f912c30e43ddbcc501862
johnhua9 <jo_hua@encs.concordia.ca>
2017-04-09 00:12:55 -0400

    Link together TAGradeFragment and AddGradeFragment

--- AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/TAGradeFragment.java
+++ AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/TAGradeFragment.java
@@ -2,9 +2,17 @@ package com.PocketMoodle;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.support.v4.app.FragmentTransaction;
+
+import java.util.*;
+
+
+import com.PocketMoodle.Services.GradesServices;
 
 /**
  * Created by John on 2017-04-03.
@@ -12,16 +20,40 @@ import android.view.ViewGroup;
 
 public class TAGradeFragment extends Fragment {
 
+    private static final String TAG = "TAGradeFragment";
+    Button openAddGrade;
+    
     public TAGradeFragment() {
         // Required empty public constructor
     }
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_grades_ta, container, false);
 
+        // Button to open the AddGrade fragment
+        openAddGrade = (Button) view.findViewById(R.id.addGrades);
+        openAddGrade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    // Fragment that will display the messages in the group
+                    AddGradeFragment tempFragment = new AddGradeFragment();
+
+                    // Start the new fragment and replace the current fragment with the new one
+                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.main_container, tempFragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+
+                } catch (Exception ex) {
+                    // In case the open fails display error message in log
+                    Log.d(TAG, "Error accessing Add Grades");
+                }
+            }
+        });
         return view;
     }
 }
