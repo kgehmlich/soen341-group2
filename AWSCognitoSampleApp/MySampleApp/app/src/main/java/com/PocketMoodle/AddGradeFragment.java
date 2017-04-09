056204fee838b5509e369801409307cddecbfd87
johnhua9 <jo_hua@encs.concordia.ca>
2017-04-09 00:11:23 -0400

    Add preliminary "AddGradeFragment"

---
+++ AWSCognitoSampleApp/MySampleApp/app/src/main/java/com/PocketMoodle/AddGradeFragment.java
@@ -0,0 +1,29 @@
+package com.PocketMoodle;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by John on 2017-04-07.
+ */
+public class AddGradeFragment extends Fragment {
+
+
+    public AddGradeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_grade, container, false);
+    }
+
+}
